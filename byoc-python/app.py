2dfc481a2b54b18b629390667696edb219ab2cde
srinathperera <hemapani@gmail.com>
2023-03-14 18:27:29 +0530

    bind to 0.0.0.0

--- byoc-python/app.py
+++ byoc-python/app.py
@@ -7,6 +7,8 @@ def hello_world():
     return 'Hello, world!'
 
 if __name__ == '__main__':
-    app.run(port=8080)
+    #app.run(port=8080)
+    #In the Python code, app.run() binds to port 8080 on localhost by default. Since you are sending requests from outside, you need to bind it to 0.0.0.0 to allow requests from outside the host.
+    app.run(host='0.0.0.0', port=8080)
 
 
